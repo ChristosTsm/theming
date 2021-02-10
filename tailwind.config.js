7f574b580ab94db0a1f11264179b6612fb1a0a83
ChristosTsm <rayden.tsm@gmail.com>
2021-02-10 10:33:46 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		spacing: {
+			1: "1px",
+			2: "2px",
+			3: "3px",
+			4: "4px",
+			5: "5px",
+			10: "10px",
+			15: "15px",
+			20: "20px",
+			25: "25px",
+			30: "30px",
+			35: "35px",
+			40: "40px",
+			45: "45px",
+			50: "50px",
+			55: "55px",
+			60: "60px",
+			auto: "auto",
+		},
+		extend: {
+			textColor: {
+				"primary-blue": "#002868",
+				"secondary-red": "#ab0427",
+				"dark-gray": "#333",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
