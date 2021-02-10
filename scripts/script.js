e6683a4f42e55990aa842baa6526a392566275c2
ChristosTsm <rayden.tsm@gmail.com>
2021-02-10 16:36:03 +0200

    mobile submenu

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// let hideSubMenu = document.querySelector("#back-to-menu");
+// let subMenuIndicator = document.querySelector("#submenu");
+
+// hideSubMenu.addEventListener("click", () => {
+// 	document.querySelector(".submenu_content").style.transform =
+// 		"translateX(500px)";
+// });
