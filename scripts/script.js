3d9edf76219d8552dff575a2828be44f5118785a
ChristosTsm <rayden.tsm@gmail.com>
2021-02-11 10:05:01 +0200

    fixed tablet menu

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-// let hideSubMenu = document.querySelector("#back-to-menu");
-// let subMenuIndicator = document.querySelector("#submenu");
-
-// hideSubMenu.addEventListener("click", () => {
-// 	document.querySelector(".submenu_content").style.transform =
-// 		"translateX(500px)";
-// });
